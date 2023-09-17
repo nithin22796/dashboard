9aae806b23f1b8c547be484b142ecafd5ccd2e47
Nithin Titus <nithin22796@gmail.com>
2023-09-17 14:47:49 +0530

    Update Routes.tsx

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,10 @@
+import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 const Layout = React.lazy(() => import("../Layout"));
 const Home = React.lazy(() => import("../pages/home"));
 const Users = React.lazy(() => import("../pages/users"));
 const Products = React.lazy(() => import("../pages/products"));
 const Login = React.lazy(() => import("../pages/login"));
-import React from "react";
 
 const route = createBrowserRouter([
   {
